7da7ef5c202bed67fd7fd9de4e5a9437cfb3cafb
Erick Pece <erick@pece.me>
2017-11-02 09:42:53 -0400

    Fixed index

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-// module.exports = require('./lib/jangomail');
-
-let JangoMail = require('./lib/jangomail');
-
-let jm = new JangoMail('YOUR USERNAME', 'YOUR PASSWORD');
-
-jm.call('AddGroup', {
-  GroupName: 'My new list'
-})
-.then(function (response) {
-  console.log(response);
-})
-.catch(function (error) {
-  console.log(error);
-});
+module.exports = require('./lib/jangomail');
